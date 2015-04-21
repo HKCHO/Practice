0119520af650ec9d5d40b1fbf205780a86f98be9
HKCHO <j880825@gmail.com>
2015-04-21 23:37:00 +0900

    android project

--- AndrodPractice/src/com/androidpractice/http/NameValuePair.java
+++ AndrodPractice/src/com/androidpractice/http/NameValuePair.java
@@ -2,7 +2,9 @@ package com.androidpractice.http;
 
 import java.util.ArrayList;
 
+import org.apache.http.NameValuePair;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.message.BasicNameValuePair;
 
 import android.app.Activity;
 
@@ -24,19 +26,19 @@ public class NameValuePair extends Activity {
 	 */
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	/*
-	 * NameValuePair은 java프로젝트에서 Dao나 Htt
+	 * NameValuePair은 서버에 요청하는 쿼리 ex)www.xxx.com?key=value;   key=value값을 묶어준것!
+	 * 
+	 * Java에서 HashMap으로 처리했다면, Android에서는 NameValuePair을 제공해준다능! 쓰기 쉽다능!
+	 * 요청 넘넘 쉽다 짱짱맨
 	 */
+	String param1 = "지적이고 잘생기기까지한 조현권";
+	String param2 = "search_type=이상형&"+"height_max=165cm&heignt_min=155cm";
 	
+	ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+	nameValuePairs.add(new BasicNameValuePair("user", param1));
+	nameValuePairs.add(new BasicNameValuePair("target", param2));	
 	
+	HttpPost request = new HttpPost();
 	
 }
