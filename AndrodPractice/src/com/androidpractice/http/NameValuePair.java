7fa57a3e3a943c649e9f841f8a5e86464983f198
HKCHO <j880825@gmail.com>
2015-04-21 23:09:47 +0900

    appcompat_v7 문제로 프로젝트 다시팜 	프로젝트 생성시 create activity 하지망! 	최소버전과 최신의 갭을 채우기 위해서 appcompat_v7프로젝트가 생성되는데 	나중에 처리하기 까다로우니 스스로 유틸을 만들어보도록

---
+++ AndrodPractice/src/com/androidpractice/http/NameValuePair.java
@@ -0,0 +1,42 @@
+package com.androidpractice.http;
+
+import java.util.ArrayList;
+
+import org.apache.http.client.methods.HttpPost;
+
+import android.app.Activity;
+
+public class NameValuePair extends Activity {
+	HttpPost request = new HttpPost();
+	
+	//nameValuePair란?
+	ArrayList<NameValuePair> nameValuePair = new ArrayList<NameValuePair>();
+	
+	//설명
+	
+	/*
+	 * NameValuePair is a special <Key, Value> pair which is used to represent parameters in http request, i.e. www.xxx.com?key=value.
+	 *
+	 * NameValuePair is an interface and is defined in apache http client, which is widely used in java to handle http operations. 
+	 * A List<NameValuePair> is just a list of <key, value> pairs, and will be used as params in http post request.
+	 * 
+	 * 끝.
+	 */
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*
+	 * NameValuePair은 java프로젝트에서 Dao나 Htt
+	 */
+	
+	
+	
+}
